22a614b7920eb255ebcf418675f07f8f46d1bbbf
Jessica <jacunnin@udel.edu>
2024-02-17 22:26:06 -0500

    added ! (trying to get pull request working)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript - Hello World
+                UD CISC275 with React Hooks and TypeScript - Hello World!
             </header>
             <p>
                 Jessica Cunningham - Edit <code>src/App.tsx</code> and save.
