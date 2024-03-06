348463abeb7d011d464cda90f904f25952e2dc25
Jessica <jacunnin@udel.edu>
2024-03-05 22:24:17 -0500

    fixed ColoredBox

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,23 +4,37 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
+interface ColorPreviewProps {
+    colorIndex: number;
+}
+
 function ChangeColor(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
+        <div>
+            <span>The current color is: {COLORS[colorIndex]}</span>
+            <div>
+                <Button
+                    onClick={() =>
+                        setColorIndex((1 + colorIndex) % COLORS.length)
+                    }
+                >
+                    Next Color
+                </Button>
+                <ColorPreview colorIndex={colorIndex}></ColorPreview>
+            </div>
+        </div>
     );
 }
 
-function ColorPreview(): JSX.Element {
+function ColorPreview({ colorIndex }: ColorPreviewProps): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[colorIndex],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -33,10 +47,8 @@ export function ColoredBox(): JSX.Element {
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
             <div>
                 <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
             </div>
         </div>
     );
