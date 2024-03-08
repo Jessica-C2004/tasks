d69bc4ae6b04acd3f7d3f458ccb027b1dfaf2c8b
Jessica <jacunnin@udel.edu>
2024-03-08 15:46:28 -0500

    pls dont fuck up anything

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import React from "react";
 import "./App.css";
 
-function App(): JSX.Element {
+function App(): void {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-        </div>
+        console.log("meh")
     );
 }
 
